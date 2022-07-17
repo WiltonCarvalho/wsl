d90ab087d32be90427212f173c813fdc12c3b352
WiltonCarvalho <45881665+WiltonCarvalho@users.noreply.github.com>
2022-07-17 14:26:50 -0300

    Create dockerd_install.sh

---
+++ dockerd_install.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+set -ex \
+# Install Docker CE
+DOCKER_BUCKET="download.docker.com"
+ARCH=x86_64
+DOCKER_VERSION=$(curl -fsSL https://${DOCKER_BUCKET}/linux/static/stable/$ARCH/ | \
+  grep -Eo '[0-9]+\.[0-9]+\.[0-9]*' | tail -1)
+curl -fsSL "https://${DOCKER_BUCKET}/linux/static/stable/$ARCH/docker-${DOCKER_VERSION}.tgz" \
+  -o docker.tgz
+tar --extract --file docker.tgz --strip-components 1  \
+  --directory /usr/local/bin/
+rm docker.tgz
+addgroup -g 133 docker || true
+# set up subuid/subgid so that "--userns-remap=default" works out-of-the-box
+addgroup dockremap || true
+id dockremap || useradd -g dockremap -s /bin/bash -d /home/dockremap -m dockremap
+grep dockremap /etc/subuid || echo 'dockremap:165536:65536' >> /etc/subuid
+grep dockremap /etc/subgid || echo 'dockremap:165536:65536' >> /etc/subgid
+# Buildx Plugin
+mkdir -p /usr/local/lib/docker/cli-plugins
+BUILDX_RELESES="https://github.com/docker/buildx/releases"
+BUILDX_VERSION=$(curl -fsL $BUILDX_RELESES | grep -m 1 -Eo 'v[0-9]+\.[0-9]+\.[0-9]*')
+curl -fsSL $BUILDX_RELESES/download/$BUILDX_VERSION/buildx-$BUILDX_VERSION.linux-amd64 \
+  -o /usr/local/lib/docker/cli-plugins/docker-buildx
+chmod +x /usr/local/lib/docker/cli-plugins/docker-buildx
+
+mkdir -p /etc/docker
+cat <<'EOF'> /etc/docker/daemon.json
+{
+  "storage-driver": "overlay2",
+  "iptables": true,
+  "ipv6": false,
+  "ip6tables": false,
+  "live-restore": false,
+  "metrics-addr": "127.0.0.1:9323",
+  "features": {
+    "buildkit": true
+  },
+  "hosts": [
+    "unix:///var/run/docker.sock",
+    "tcp://127.0.0.1:2376"
+  ],
+  "pidfile": "/var/run/docker.pid",
+  "group": "docker"
+}
+EOF
+
+sed -i "/group/ s/docker/$SUDO_USER/" /etc/docker/daemon.json
+
+docker --version
